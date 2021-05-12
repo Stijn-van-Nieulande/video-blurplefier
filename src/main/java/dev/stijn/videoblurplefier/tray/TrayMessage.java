5c54b9f681ce9389a12c5de33f6a4b8e99b2f7ca
Sticks <tsommers150@gmail.com>
2021-05-11 20:15:30 -0400

    Tray Handler Added

---
+++ src/main/java/dev/stijn/videoblurplefier/tray/TrayMessage.java
@@ -0,0 +1,33 @@
+package dev.stijn.videoblurplefier.tray;
+
+import javax.swing.*;
+import java.awt.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+
+public class traymsg
+{
+    public static void main(String[] args, TrayIcon.MessageType type, String message) throws AWTException, MalformedURLException {
+        if (SystemTray.isSupported()) {
+            displayTray(message, type);
+        } else {
+            System.err.println("System tray not supported!");
+        }
+    }
+
+    public static void displayTray(String message, TrayIcon.MessageType type) throws AWTException, MalformedURLException {
+
+        URL imageres = traymsg.class.getClassLoader().getResource("resources/trayicon.png");
+        Image image = new ImageIcon(imageres).getImage();
+        //Obtain only one instance of the SystemTray object
+        SystemTray tray = SystemTray.getSystemTray();
+        TrayIcon trayIcon = new TrayIcon(image, "Tray Demo");
+
+        trayIcon.setImageAutoSize(true);
+        //Set tooltip text for the tray icon
+        trayIcon.setToolTip("System tray icon demo");
+        tray.add(trayIcon);
+        trayIcon.displayMessage("Video Blurplefier", message, type);
+    }
+}
